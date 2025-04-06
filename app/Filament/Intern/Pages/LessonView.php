f840d87de43f4c034413269a42700f339b64d841
Pavel Ivanov <p.ivanov.spb@gmail.com>
2025-04-06 12:23:10 +0300

    Добавлена страница LessonView для просмотра уроков
    Реализована новая страница Filament — LessonView, предназначенная для отображения деталей урока, включая содержание, видео и навигацию по курсу. Реализация включает представление Blade и серверную логику для загрузки данных урока и связанных записей на курс.

---
+++ app/Filament/Intern/Pages/LessonView.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Filament\Intern\Pages;
+
+use App\Models\Enrollment;
+use App\Models\Lesson;
+use Filament\Pages\Page;
+use Illuminate\Database\Eloquent\Model;
+
+class LessonView extends Page
+{
+    protected static bool $shouldRegisterNavigation = false;
+    protected static ?string $title = '';
+    protected static string $view = 'filament.intern.pages.lesson-view';
+    protected static ?string $slug = 'lessons';
+
+    public Model | int | string | null $record;
+    public Enrollment $enrollment;
+
+    public function mount(int|string $record)
+    {
+        $this->record = Lesson::findOrFail($record);
+        $this->enrollment = Enrollment::where('course_id', $this->record->course->id)
+            ->where('user_id', auth()->id())
+            ->with('steps')
+            ->firstOrFail();
+
+    }
+
+    public static function getRoutePath(): string
+    {
+        return '/' . static::getSlug() . '/{record}';
+    }
+
+}
