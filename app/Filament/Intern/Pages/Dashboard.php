994d91561b441b9ef3864a06da610a817e1df90d
Pavel Ivanov <p.ivanov.spb@gmail.com>
2025-04-08 12:07:23 +0300

    Update Dashboard page layout and widgets for Intern users
    Changed the navigation icon, heading, and label to enhance clarity. Updated the column layout to single-column and added the "MyTestsResults" widget for better user content organization.

--- app/Filament/Intern/Pages/Dashboard.php
+++ app/Filament/Intern/Pages/Dashboard.php
@@ -3,23 +3,27 @@
 namespace App\Filament\Intern\Pages;
 
 use App\Filament\Intern\Widgets\AssignedCoursesWidget;
+use App\Filament\Intern\Widgets\MyTestsResults;
 use Filament\Pages\Page;
 use Filament\Pages\Dashboard as BaseDashboard;
 
 class Dashboard extends BaseDashboard
 {
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static ?string $navigationIcon = 'heroicon-o-home';
 //    protected static string $view = 'filament.intern.pages.dashboard';
+    protected ?string $heading = 'Обзор';
+    protected static ?string $navigationLabel = 'Обзор';
 
     public function getColumns(): int | array
     {
-        return 2;
+        return 1;
     }
 
     public function getWidgets(): array
     {
         return [
             AssignedCoursesWidget::class,
+            MyTestsResults::class
         ];
     }
 }
