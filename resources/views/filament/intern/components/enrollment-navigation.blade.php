41ab4920ebbba49740be5f73d326696f0498252f
Pavel Ivanov <p.ivanov.spb@gmail.com>
2025-04-28 10:41:35 +0300

    Refactor enrollment navigation to use semantic <nav> element.
    Replaced the <div> wrapper with a semantic <nav> and added a TODO comment suggesting dynamic component rendering for improved performance. These changes enhance accessibility and prepare for future optimizations.

--- resources/views/filament/intern/components/enrollment-navigation.blade.php
+++ resources/views/filament/intern/components/enrollment-navigation.blade.php
@@ -1,7 +1,13 @@
-<div>
+<nav class="navigation">
     <ul role="list" class="divide-y divide-gray-200">
         @foreach($navigation as $item)
             @include($item['template'], $item)
+{{--    ToDo: Dynamic component rendering instead of include for better performance.
+            <x-dynamic-component
+                :component="$item['template']"
+                :attributes="$item"
+            />
+--}}
         @endforeach
     </ul>
-</div>
+</nav>
