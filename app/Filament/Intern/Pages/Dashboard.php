981eb6bed0166a85058136ebea5330fec0fe025c
Pavel Ivanov <p.ivanov.spb@gmail.com>
2025-04-29 12:14:51 +0300

    Update Dashboard labels and icon to reflect 'Academy'
    Changed the navigation icon, heading, and navigation label to align with the updated 'Academy' theme. These changes provide a clearer and more relevant UI for the targeted user roles.

--- app/Filament/Intern/Pages/Dashboard.php
+++ app/Filament/Intern/Pages/Dashboard.php
@@ -9,10 +9,10 @@ use Filament\Pages\Dashboard as BaseDashboard;
 
 class Dashboard extends BaseDashboard
 {
-    protected static ?string $navigationIcon = 'heroicon-o-home';
+    protected static ?string $navigationIcon = 'heroicon-o-academic-cap';
 //    protected static string $view = 'filament.intern.pages.dashboard';
-    protected ?string $heading = 'Обзор';
-    protected static ?string $navigationLabel = 'Обзор';
+    protected ?string $heading = 'Академия';
+    protected static ?string $navigationLabel = 'Академия';
 
     public function getColumns(): int | array
     {
