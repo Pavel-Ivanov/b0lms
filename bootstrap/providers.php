addc564d5e48ac12a52f6cfd4b0452779f5a792c
Pavel Ivanov <p.ivanov.spb@gmail.com>
2025-05-28 22:19:25 +0300

    Remove StudentPanelProvider from providers list
    The StudentPanelProvider has been removed from the providers array to streamline the application's service providers. This change helps eliminate unused or redundant services, improving maintainability.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,4 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\InternPanelProvider::class,
     App\Providers\Filament\SadminPanelProvider::class,
-    App\Providers\Filament\StudentPanelProvider::class,
 ];
