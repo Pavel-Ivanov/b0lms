f8f2985f19fa85a34d5eddaa01eb55891fb6e71a
Pavel Ivanov <p.ivanov.spb@gmail.com>
2025-08-02 11:32:39 +0300

    Comment out "Mark as Completed" button in lesson content for future use

--- resources/views/filament/pages/intern/lesson-content.blade.php
+++ resources/views/filament/pages/intern/lesson-content.blade.php
@@ -6,6 +6,7 @@
 --}}
 <div>
     @if ($lesson)
+{{--
         <div class="flex justify-end">
             <x-filament::button type="button"
                 wire:click="markLessonAsCompleted"
@@ -13,6 +14,7 @@
                 Отметить как завершенный
             </x-filament::button>
         </div>
+--}}
 
         <div class="border-b border-gray-200 pb-4">
             <h2 class="text-xl font-semibold text-gray-900">
