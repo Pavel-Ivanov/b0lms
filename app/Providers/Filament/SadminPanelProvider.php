3c913368079a272731fc78402406251534b3d247
Pavel Ivanov <p.ivanov.spb@gmail.com>
2025-09-05 08:12:27 +0700

    Add default sorting by `name` in department and position resources

--- app/Providers/Filament/SadminPanelProvider.php
+++ app/Providers/Filament/SadminPanelProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers\Filament;
 
 use App\Filament\Pages\Sadmin\Backups;
+use App\Filament\Sadmin\Resources\LessonResource;
 use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
+use CharrafiMed\GlobalSearchModal\GlobalSearchModalPlugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -94,6 +96,13 @@ class SadminPanelProvider extends PanelProvider
                     ->usingPage(Backups::class)
                     ->usingPolingInterval('60s')
                     ->timeout(120),
+                GlobalSearchModalPlugin::make()
+//                    ->scopes(LessonResource::class)
+                    ->highlightQueryStyles([
+                        'background-color' => 'yellow',
+                        'font-weight' => 'bold',
+                    ])
+                ,
             ])
 //            ->viteTheme('resources/css/filament/sadmin/theme.css')
 //            ->viteTheme('"C:\Herd\b0lms\resources\css\filament\sadmin\theme.css"')
