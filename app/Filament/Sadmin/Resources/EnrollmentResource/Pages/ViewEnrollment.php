2f2d1459704ac7db4ac289523a5cf49ffd37e8f1
Pavel Ivanov <p.ivanov.spb@gmail.com>
2025-09-11 11:30:21 +0700

    refactor(enrollment): move `infoList` method from EnrollmentResource to ViewEnrollment

--- app/Filament/Sadmin/Resources/EnrollmentResource/Pages/ViewEnrollment.php
+++ app/Filament/Sadmin/Resources/EnrollmentResource/Pages/ViewEnrollment.php
@@ -3,7 +3,11 @@
 namespace App\Filament\Sadmin\Resources\EnrollmentResource\Pages;
 
 use App\Filament\Sadmin\Resources\EnrollmentResource;
+use App\Models\EnrollmentStep;
 use Filament\Actions;
+use Filament\Infolists\Components\RepeatableEntry;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewEnrollment extends ViewRecord
@@ -34,4 +38,72 @@ class ViewEnrollment extends ViewRecord
                 }),
         ];
     }
+
+    public function infoList(Infolist $infolist): Infolist
+    {
+        return $infolist
+
+            ->schema([
+                \Filament\Infolists\Components\Tabs::make('Tabs')
+                    ->tabs([
+                        \Filament\Infolists\Components\Tabs\Tab::make('Основная информация')
+                            ->schema([
+                                TextEntry::make('course.name')
+                                    ->label('Курс')
+                                    ->inlineLabel(),
+                                TextEntry::make('user.name')
+                                    ->label('Студент')
+                                    ->inlineLabel(),
+                                TextEntry::make('enrollment_date')
+                                    ->label('Дата назначения')
+                                    ->inlineLabel(),
+                                TextEntry::make('completion_deadline')
+                                    ->label('Дата окончания обучения')
+                                    ->inlineLabel(),
+
+                            ]),
+                        \Filament\Infolists\Components\Tabs\Tab::make('План обучения')
+                            ->schema([
+                                RepeatableEntry::make('steps')
+                                    ->hiddenLabel()
+                                    ->schema([
+                                        \Filament\Infolists\Components\Section::make('step')
+                                            ->heading(function (EnrollmentStep $record) {
+                                                return $record->stepableModel()->name;
+                                            })
+                                            ->schema([
+                                                TextEntry::make('is_completed')
+                                                    ->label('Статус:')
+                                                    ->inlineLabel()
+//                                                    ->hiddenLabel()
+                                                    ->formatStateUsing(function ($state) {
+                                                        return $state === true ? 'Завершен' : 'Не завершен';
+                                                    }),
+                                                TextEntry::make('started_at')
+                                                    ->label('Начат:')
+                                                    ->inlineLabel(),
+                                                TextEntry::make('completed_at')
+                                                    ->label('Завершен:')
+                                                    ->inlineLabel(),
+
+                                            ]),
+                                    ])
+                                    ->contained(false),
+
+                            ]),
+                    ])
+                    ->persistTab()
+                    ->columnSpan('full')
+                    ->activeTab(1),
+
+                /*                \Filament\Infolists\Components\Section::make('Основная информация')
+                                    ->schema([
+                                    ]),
+                            \Filament\Infolists\Components\Section::make('План обучения')
+                                ->schema([
+                                ])
+                            ->columns(),*/
+            ]);
+    }
+
 }
