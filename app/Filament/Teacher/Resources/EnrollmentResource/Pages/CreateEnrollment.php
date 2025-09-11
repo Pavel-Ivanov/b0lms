2d97d518a7f1c1cfd2b14e3d5f224b3f464dcec6
Pavel Ivanov <p.ivanov.spb@gmail.com>
2025-09-11 08:45:45 +0700

    Replace `TiptapEditor` with `TinyEditor` in lesson content schema of `LessonResource`

--- app/Filament/Teacher/Resources/EnrollmentResource/Pages/CreateEnrollment.php
+++ app/Filament/Teacher/Resources/EnrollmentResource/Pages/CreateEnrollment.php
@@ -4,9 +4,59 @@ namespace App\Filament\Teacher\Resources\EnrollmentResource\Pages;
 
 use App\Filament\Teacher\Resources\EnrollmentResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateEnrollment extends CreateRecord
 {
     protected static string $resource = EnrollmentResource::class;
+    protected static bool $canCreateAnother = false;
+
+    protected function getRedirectUrl(): string
+    {
+        return self::getResource()::getUrl('index');
+    }
+
+    protected function beforeCreate(): void
+    {
+        $data = $this->form->getState();
+
+        if (\App\Models\Enrollment::hasIncompleteEnrollment($data['course_id'], $data['user_id'])) {
+            Notification::make()
+                ->danger()
+                ->title('Ошибка создания назначения')
+                ->body('Уже существует незавершенное назначение этого курса для данного студента.')
+                ->persistent()
+                ->send();
+
+            $this->halt();
+        }
+    }
+
+    protected function afterCreate(): void
+    {
+        $result = $this->record->createLearningPlan();
+
+        if ($result['success']) {
+            Notification::make()
+                ->success()
+                ->title($result['message'])
+                ->body("Создано {$result['count']} шагов.")
+                ->send();
+        } else {
+            // Delete the enrollment if learning plan creation failed
+            $this->record->delete();
+
+            Notification::make()
+                ->danger()
+                ->title('Ошибка создания назначения')
+                ->body($result['message'])
+                ->persistent()
+                ->send();
+
+            $this->halt();
+        }
+
+    }
+
 }
