fe5f24d1fa53e2af90ced624c868adcd31d1d7d3
Pavel Ivanov <p.ivanov.spb@gmail.com>
2025-09-13 14:22:20 +0700

    refactor(enrollment): update `status` filter label in EnrollmentResource to `Состояние`

--- resources/views/filament/teacher/components/enrollment-step-heading.blade.php
+++ resources/views/filament/teacher/components/enrollment-step-heading.blade.php
@@ -6,7 +6,8 @@
     />
 
     <div class="flex flex-col">
-        <span>{{ $type }} {{ isset($courseName) ? $courseName : $text }}</span>
+{{--        <span>{{ $type }} {{ isset($courseName) ? $courseName : $text }}</span>--}}
+        <span>{{ isset($courseName) ? $courseName : $text }}</span>
         @if(isset($parameters))
             <span class="text-gray-500 text-sm">{{ $parameters }}</span>
         @endif
